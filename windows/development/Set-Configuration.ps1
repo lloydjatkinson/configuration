2007f2246aa5573d6e26aa48ca839ccc0544d053
Lloyd Atkinson <lloydjatkinson@gmail.com>
2023-03-18 00:33:23 +0000

    windows/development: hyper-v features etc

--- windows/development/Set-Configuration.ps1
+++ windows/development/Set-Configuration.ps1
@@ -7,66 +7,7 @@ function Invoke-SoftwareInstallationWithWinGet {
     echo Y | winget list | Out-Null
     
     winget install --id Microsoft.Powershell --source winget
-    winget install --id=Google.Chrome -e
-    winget install --id=Spotify.Spotify -e
-    winget install --id=WhatsApp.WhatsApp -e
-    winget install --id=VideoLAN.VLC  -e
-    winget install --id=Valve.Steam -e 
-    winget install --id=dotPDNLLC.paintdotnet -e
-    winget install --id=Nota.Gyazo  -e
-    winget install --id=OBSProject.OBSStudio -e
-    winget install --id=qBittorrent.qBittorrent -e
-    winget install --id=Obsidian.Obsidian -e
-    winget install --id=Microsoft.PowerToys --source winget
-    winget install --id=emoacht.Monitorian -e 
-}
 
-function Set-Taskbar {
-    Write-Host "🛠️ Setting taskbar layout and items" -ForegroundColor Green
-
-    $registryKeys = @(
-        @{
-            Path = "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-            Name = "TaskbarDa"
-            Value = 0
-        },
-        @{
-            Path = "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-            Name = "TaskbarMn"
-            Value = 0
-        },
-        @{
-            Path = "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-            Name = "TaskbarAl"
-            Value = 0
-        },
-        @{
-            Path = "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search"
-            Name = "SearchboxTaskbarMode"
-            Value = 0
-        }
-    )
-
-    foreach ($key in $registryKeys) {
-        [Microsoft.Win32.Registry]::SetValue($key.Path, $key.Name, $key.Value)
-    }
-
-    Stop-Process -Name "explorer"
-}
-
-function Show-FileExtensions {
-    Write-Host "🛠️ Showing file extensions" -ForegroundColor Green
-    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -Value 0
-}
-
-function Hide-OneDriveExplorerIcon {
-    Write-Host "🛠️ Hiding OneDrive icon in Explorer navigation pane" -ForegroundColor Green
-    [Microsoft.Win32.Registry]::SetValue("HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}", "System.IsPinnedToNameSpaceTree", 0)
-}
-
-function Hide-DesktopIcons {
-    Write-Host "🛠️ Hiding desktop icons" -ForegroundColor Green
-    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideIcons' -Value 1
 }
 
 function Set-WindowsTerminalDefaultProfile {
@@ -78,24 +19,20 @@ function Set-WindowsTerminalDefaultProfile {
     $jsonContent | ConvertTo-Json -Depth 32 | Set-Content -Path $jsonFilePath
 }
 
-function Get-WindowsUpdates {
-    Set-ExecutionPolicy Unrestricted
-    Install-PackageProvider -Name NuGet -Force
-    Install-Module PSWindowsUpdate -Force
-    Import-Module PSWindowsUpdate
-    Get-WindowsUpdate -AcceptAll -Install
-    Set-ExecutionPolicy Restricted
+function Set-HyperVAndWsl {
+    Write-Host "🖥️ Enabling Hyper-V, WSL, Sandbox, NFS" -ForegroundColor Green
+    Enable-WindowsOptionalFeature -Online -FeatureName "Containers-DisposableClientVM" -All -NoRestart
+    Enable-WindowsOptionalFeature -Online -FeatureName "HypervisorPlatform" -All -NoRestart
+    Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -All -NoRestart
+    Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V" -All -NoRestart
+    Enable-WindowsOptionalFeature -Online -FeatureName ServicesForNFS-ClientOnly, ClientForNFS-Infrastructure, NFS-Administration
 }
 
-function Invoke-StandardSetup {
+function Invoke-DevelopmentSetup {
     try {
         Invoke-SoftwareInstallationWithWinGet
-        Show-FileExtensions
-        Hide-OneDriveExplorerIcon
-        Hide-DesktopIcons
-        Set-Taskbar
         Set-WindowsTerminalDefaultProfile
-        Get-WindowsUpdates
+        Set-HyperVAndWsl
 
         (New-Object System.Media.SoundPlayer $(Get-ChildItem -Path "$env:windir\Media\Windows Logon.wav").FullName).Play()
         Write-Host "✅ Windows configured and software installed OK" -ForegroundColor Green
@@ -108,4 +45,4 @@ function Invoke-StandardSetup {
     }
 }
 
-Invoke-StandardSetup
+Invoke-DevelopmentSetup
